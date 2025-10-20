c91eea26fd026584d96fdcfcb333ed0ca8277ace
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-10-20 12:07:33 +0700

    menambah 1 matkul

--- CM1_06.java
+++ CM1_06.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class CM1_06 {
@@ -31,17 +30,28 @@ public class CM1_06 {
         System.out.print("Nilai Tugas\t: ");
         double nilaiTugas2 = sc.nextDouble();
 
+        /* Input Nilai UTS, nilai UAS, dan nilai tugas pada mata kuliah 3 */
+        System.out.println("\n--- Mata Kuliah 3: Rekayasa Perangkat Lunak ---");
+        System.out.print("Nilai UTS\t: ");
+        double nilaiUts3 = sc.nextDouble();
+        System.out.print("Nilai UAS\t: ");
+        double nilaiUas3 = sc.nextDouble();
+        System.out.print("Nilai Tugas\t: ");
+        double nilaiTugas3 = sc.nextDouble();
+
         /* HITUNG NILAI AKHIR */
         double nilaiAkhir1 = (nilaiUts1 * 0.3) + (nilaiUas1 * 0.4) + (nilaiTugas1 * 0.3);
         double nilaiAkhir2 = (nilaiUts2 * 0.3) + (nilaiUas2 * 0.4) + (nilaiTugas2 * 0.3);
-        double nilaiRataRata = (nilaiAkhir1 + nilaiAkhir2) / 2;
+        double nilaiAkhir3 = (nilaiUts3 * 0.3) + (nilaiUas3 * 0.4) + (nilaiTugas3 * 0.3);
+        double nilaiRataRata = (nilaiAkhir1 + nilaiAkhir2 + nilaiAkhir3) / 3;
 
         /* Status nilai akhir 1 dan 2 (lulus/tidak lulus) */
         String status1 = (nilaiAkhir1 >= 60) ? "Lulus" : "Tidak Lulus";
         String status2 = (nilaiAkhir2 >= 60) ? "Lulus" : "Tidak Lulus";
+        String status3 = (nilaiAkhir3 >= 60) ? "Lulus" : "Tidak Lulus";
 
         /* Konversi nilai huruf (Switch Case) */
-        String huruf1, huruf2, kualifikasi;
+        String huruf1, huruf2, huruf3, kualifikasi;
         double nilaiSetara;
 
         /* Kategori Nilai Huruf Matkul 1 */
@@ -110,23 +120,44 @@ public class CM1_06 {
             huruf2 = "Invalid";
         }
 
-        if (nilaiAkhir1 >= 60) {
-            status1 = "Lulus";
+        /* Kategori Nilai Huruf Matkul 3 */
+        if (80 < nilaiAkhir3 && nilaiAkhir3 <= 100) {
+            huruf3 = "A";
+            nilaiSetara = 4.0;
+            kualifikasi = "Sangat baik";
+        } else if (73 < nilaiAkhir3 && nilaiAkhir3 <= 80) {
+            huruf3 = "B+";
+            nilaiSetara = 3.5;
+            kualifikasi = "Lebih dari baik";
+        } else if (65 < nilaiAkhir3 && nilaiAkhir3 <= 73) {
+            huruf3 = "B";
+            nilaiSetara = 3;
+            kualifikasi = "Baik";
+        } else if (60 < nilaiAkhir3 && nilaiAkhir3 <= 65) {
+            huruf3 = "C+";
+            nilaiSetara = 2.5;
+            kualifikasi = "Lebih dari cukup";
+        } else if (50 < nilaiAkhir3 && nilaiAkhir3 <= 60) {
+            huruf3 = "C";
+            nilaiSetara = 2;
+            kualifikasi = "Cukup";
+        } else if (39 < nilaiAkhir3 && nilaiAkhir3 <= 50) {
+            huruf3 = "D";
+            nilaiSetara = 1;
+            kualifikasi = "Kurang";
+        } else if (nilaiAkhir3 >= 0 && nilaiAkhir3 <= 39) {
+            huruf3 = "E";
+            nilaiSetara = 0;
+            kualifikasi = "Gagal";
         } else {
-            status1 = "Tidak Lulus";
-        }
-
-        if (nilaiAkhir2 >= 60) {
-            status2 = "Lulus";
-        } else {
-            status2 = "Tidak Lulus";
+            huruf3 = "Invalid";
         }
 
         /* Rata-rata dan Status Semester (Nested if) */
-        double rataRata = (nilaiAkhir1 + nilaiAkhir2) / 2;
+        double rataRata = (nilaiAkhir1 + nilaiAkhir2 + nilaiAkhir3) / 3;
         String statusSemester;
 
-        if (status1.equals("Lulus") && status2.equals("Lulus")) {
+        if (status1.equals("Lulus") && status2.equals("Lulus") && status3.equals("Lulus")) {
             if (rataRata >= 70) {
                 statusSemester = "Lulus";
             } else {
@@ -142,8 +173,9 @@ public class CM1_06 {
         System.out.println("NIM\t: " + NIM);
         System.out.println("\nMata Kuliah\t\t\t\tUTS\tUAS\tTugas\tNilai Akhir\t\tNilai Huruf\t\tStatus");
         System.out.println("-------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("Algoritma Pemrograman\t\t\t" + nilaiUts1 + "\t" + nilaiUas1 + "\t" + nilaiTugas1 + "\t" + nilaiAkhir1 + "\t\t\t" + huruf1 + "\t\t" + status1);
-        System.out.println("Struktur Data\t\t\t\t" + nilaiUts2 + "\t" + nilaiUas2 + "\t" + nilaiTugas2 + "\t" + nilaiAkhir2 + "\t\t\t" + huruf2 + "\t\t" + status2);
+        System.out.println("Algoritma Pemrograman\t\t\t" + nilaiUts1 + "\t" + nilaiUas1 + "\t" + nilaiTugas1 + "\t" + nilaiAkhir1 + "\t\t\t" + huruf1 + "\t\t\t" + status1);
+        System.out.println("Struktur Data\t\t\t\t" + nilaiUts2 + "\t" + nilaiUas2 + "\t" + nilaiTugas2 + "\t" + nilaiAkhir2 + "\t\t\t" + huruf2 + "\t\t\t" + status2);
+        System.out.println("Rekayasa Perangkat Lunak\t\t" + nilaiUts3 + "\t" + nilaiUas3 + "\t" + nilaiTugas3 + "\t" + nilaiAkhir3 + "\t\t\t" + huruf3 + "\t\t\t" + status3);
         System.out.println("\nRata-rata Nilai Akhir: " + rataRata);
         System.out.println("Status Semester: " + statusSemester);
     }
